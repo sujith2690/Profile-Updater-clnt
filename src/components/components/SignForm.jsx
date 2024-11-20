fcc31a63944eaa7b39d8ddaf64aaf25594315aa0
Sujith P <sujithp2690@gmail.com>
2024-11-21 01:36:10 +0530

    signup page completed

--- src/components/components/SignForm.jsx
+++ src/components/components/SignForm.jsx
@@ -1,42 +1,110 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { signUpSchema } from '../../schemas/validationSchema';
+import { signUpApi } from '../../API/authApis';
+import { useFormik } from 'formik';
 
 const SignForm = ({ location }) => {
-    console.log(location, '-----------location')
+    const [formValues, setFormValues] = useState({
+        userName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+    const [loading, setLoading] = useState(false)
+    const initialValues = formValues
+    const validationSchema = signUpSchema
+    const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit: async (values, action) => {
+            setLoading(true)
+            try {
+                const result = await signUpApi(values)
+                navigate('/')
+            } catch (error) {
+                console.log(error, 'Login failed');
+            }
+            action.resetForm();
+            console.log('before reset')
+            setLoading(false)
+        }
+    })
     return (
         <div className="p-4 bg-white shadow rounded" style={{ width: '500px' }}>
             <h2 className="text-center mb-4">{location !== 'admin' ? 'Sign Up' : 'Admin SignUp'}</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3 d-flex gap-2">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Username"
-                        required
-                    />
-                    <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Email"
-                        required
-                    />
+                    <div>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Username"
+                            required
+                            id='userName'
+                            value={values.userName}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {errors.userName && touched.userName ? (
+                            <p className="text-danger">{errors.userName}</p>
+                        ) : null}
+                    </div>
+                    <div>
+                        <input
+                            type="email"
+                            className="form-control"
+                            placeholder="Email"
+                            required
+                            id='email'
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {errors.email && touched.email ? (
+                            <p className="text-danger">{errors.email}</p>
+                        ) : null}
+                    </div>
                 </div>
                 <div className="mb-3 d-flex gap-2">
-                    <input
-                        type="password"
-                        className="form-control"
-                        placeholder="Password"
-                        required
-                    />
-                    <input
-                        type="password"
-                        className="form-control"
-                        placeholder="Confirm Password"
-                        required
-                    />
+                    <div>
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Password"
+                            required
+                            id='password'
+                            value={values.password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {errors.password && touched.password ? (
+                            <p className="text-danger">{errors.password}</p>
+                        ) : null}
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Confirm Password"
+                            required
+                            id='confirmPassword'
+                            value={values.confirmPassword}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {errors.confirmPassword && touched.confirmPassword ? (
+                            <p className="text-danger">{errors.confirmPassword}</p>
+                        ) : null}
+                    </div>
                 </div>
                 <button type="submit" className="btn btn-primary w-100">
-                    SignUp
+                    {
+                        loading ?
+                            <div className="spinner-border" role="status">
+                                <span className="sr-only"></span>
+                            </div> : 'SignUp'
+                    }
                 </button>
             </form>
             <p className="text-center mt-3">
--- src/schemas/validationSchema.js
+++ src/schemas/validationSchema.js
@@ -4,7 +4,7 @@ export const loginSchema = Yup.object({
     password: Yup.string().min(3).required("Enter Your Password")
 })
 export const signUpSchema = Yup.object({
-    name: Yup.string().min(1).max(25).required("Please enter your Name"),
+    userName: Yup.string().min(1).max(25).required("Please enter your Name"),
     email: Yup.string().email().required("Please enter your Email"),
     password: Yup.string().min(3).required("Please enter your Password"),
     confirmPassword: Yup.string().required().oneOf([Yup.ref('password'), null], "Password Must match")
